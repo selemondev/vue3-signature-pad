f19ff28111cc56febc49799369012fc2ce5d0337
selemondev <selemondev19@gmail.com>
2024-06-30 20:59:53 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as VueSignaturePad } from './components/VueSignaturePad.vue'
+export type { CanvasOptions, Props, WaterMarkObj } from './types'
---
+++ src/types/index.ts
@@ -0,0 +1,54 @@
+export interface Options {
+  penColor: string
+  backgroundColor: string
+}
+
+export interface WaterMarkObj {
+  text: string
+  font: string
+  style: string
+  fillStyle: string
+  strokeStyle: string
+  x: number
+  y: number
+  sx: number
+  sy: number
+}
+
+export interface SignatureRef {
+  addEventListener?: (type: string, listener: (event: Event) => void, options?: { once?: boolean }) => void
+  addWaterMark?: (obj: WaterMarkObj) => void
+  fromDataURL?: (url: string) => void
+  toDataURL?: (format?: string) => void
+  save?: (t: string) => void
+  clear?: () => void
+  isEmpty?: () => void
+  undo?: () => void
+  off?: () => void
+  on?: () => void
+  minWidth?: number
+  maxWidth?: number
+  penColor?: string
+}
+
+export interface Props {
+  options: Options
+  width: string
+  height: string
+  minWidth?: number
+  maxWidth?: number
+  waterMark?: WaterMarkObj
+  throttle?: number
+  disabled?: boolean
+  clearOnResize?: boolean
+  defaultUrl?: string
+}
+
+export interface CanvasOptions {
+  signaturePad: SignatureRef
+  throttle: number
+  minWidth?: number
+  maxWidth?: number
+  option: Options
+  canvasUuid: string
+}
