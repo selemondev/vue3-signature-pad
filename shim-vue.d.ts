f069b3e9fbbea4ccdc5c134270e80ef3682e8a4b
selemondev <selemondev19@gmail.com>
2024-07-02 11:32:18 +0300

    chore: lint

--- shim-vue.d.ts
+++ shim-vue.d.ts
@@ -1,5 +1,6 @@
-declare module "*.vue" {
-    import { defineComponent } from "vue";
-    const component: ReturnType<typeof defineComponent>;
-    export default component;
+declare module '*.vue' {
+  import type { defineComponent } from 'vue'
+
+  const component: ReturnType<typeof defineComponent>
+  export default component
 }
